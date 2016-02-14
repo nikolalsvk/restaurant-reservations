288193d078528084f85703ab4fa5b6ef43f856d4
nikolalsvk <nikolaseap@gmail.com>
2016-02-14 20:03:25 +0100

    Add before_create action to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_user_role
+
+  private
+
+  def set_user_role
+    self.role = "user"
+  end
+
 end
